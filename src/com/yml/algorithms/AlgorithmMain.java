f8f713f2b9ef6549ada331b550240a145abb3b21
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-19 23:17:09 +0530

    Binary search algorithm implemented

---
+++ src/com/yml/algorithms/AlgorithmMain.java
@@ -0,0 +1,9 @@
+package com.yml.algorithms;
+
+import java.io.FileNotFoundException;
+
+public class AlgorithmMain {
+    public static void main(String args[]) throws FileNotFoundException {
+        BinarySearch.run();
+    }
+}
